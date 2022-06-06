02b92c91eefc25edd23acf9901b5df64d9ac7eed
HimanshuNutan <57312592+HimanshuNutan@users.noreply.github.com>
2022-06-06 18:13:25 +0530

    Add files via upload

---
+++ Java22_ArrayRemoveDuplicates.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class Java22_ArrayRemoveDuplicates {
+    public static void main(String[] args) {
+        int ar[] = {1,2,1,2,3,8,7,9,2,3,4,5,6,6,6,7,8,8,9};
+        HashSet<Integer> set = new HashSet<>();
+        for(int i=0;i<ar.length;i++){
+           if(!set.contains(ar[i])){
+               set.add(ar[i]);
+           }
+        }
+        int j=0;
+        for(int i:set){
+            ar[j]=i;j++;
+        }
+    }
+}
